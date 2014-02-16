29c6ff5b6c359da3c72272ccca965a6c7b4cede6
Rich <richard.abdill@gmail.com>
2014-02-16 15:46:00 -0500

    Fixed production bootstrap problems
    Errors from creating duplicate budgets

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -29,17 +29,14 @@ class BootStrap {
 		
 		
 		//	Making a sample budget:
-		def richard = new Budget(name: "RichBudg",user:rabdillUser)
+		def richard = Budget.findByName('RichBudg') ?: new Budget(
+			name: "RichBudg",user:rabdillUser)
 		.addToTransactions(date: "12/25/2013", description: "Xmas", amount: "1000")		
 		.addToTransactions(date: "12/31/2013", description: "found in road", amount: "203")
 		.addToTransactions(date: "12/03/2013", description: "starter", amount: "20")
 		.addToTransactions(date: "01/09/2014", description: "stereo", amount: "-550")
 		.addToAccounts(name: "Student loan", startValue: "5051", multiplier: "1").save(failOnError: true)
-		
-		def creditCard = new Account(name: "Credit card", startValue: "1490", multiplier: "1", budget:richard).save(failOnError: true)
-		
-		def ccPay = new Transaction(date:"03/25/2014", description: "cc payment", amount: "-1000", budget:richard, accountLink:creditCard.id).save(failOnError: true)
-		
+			
     }
     def destroy = {
     }
